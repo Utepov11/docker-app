b9f3805abac24b324c18e314b426f57f752e83ac
Utepov11 <Utepov.Erzhan@gmail.com>
2025-03-10 20:42:47 +0500

    second

--- my_docker_app/my_docker_app/settings.py
+++ my_docker_app/my_docker_app/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "rU1kyk$44(=Hu1IwG"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
