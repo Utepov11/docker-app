c1f04271f7edec4e8c3d34e0cff75c365158e559
Utepov11 <Utepov.Erzhan@gmail.com>
2025-03-11 15:56:32 +0500

    second

--- my_docker_app/my_docker_app/settings.py
+++ my_docker_app/my_docker_app/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
-123 = adsada
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +10,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+# Инициализация
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# Чтение .env файла
+environ.Env.read_env()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
