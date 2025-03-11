a8b71419abe194fc79d81d5793d113ac50772766
Utepov11 <Utepov.Erzhan@gmail.com>
2025-03-11 16:18:25 +0500

    env

--- my_docker_app/my_docker_app/settings.py
+++ my_docker_app/my_docker_app/settings.py
@@ -13,9 +13,8 @@ environ.Env.read_env(BASE_DIR / '.env')
 
 # Чтение настроек из .env
 SECRET_KEY = env("DJANGO_SECRET_KEY")  # Используем django-environ для чтения ключа
-DEBUG = env("DEBUG")
+DEBUG = int(environ.get("DEBUG", default=0))
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["127.0.0.1"])
-
 
 
 # Application definition
