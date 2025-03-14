f95a40453f07fb8e5a319d3dd60e0eab198b2610
Utepov11 <Utepov.Erzhan@gmail.com>
2025-03-14 10:29:20 +0500

    I change DB

--- my_docker_app/my_docker_app/settings.py
+++ my_docker_app/my_docker_app/settings.py
@@ -64,17 +64,23 @@ WSGI_APPLICATION = 'my_docker_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'devsrch',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Chicago23',
+#         'HOST': 'db',
+#         'PORT': '5433',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'devsrch',
-        'USER': 'postgres',
-        'PASSWORD': 'Chicago23',
-        'HOST': 'db',
-        'PORT': '5433',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
